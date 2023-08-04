33217b814f6dbb1a715b1561ff0179ccc750d947
LolAsMe <stared@ymail.com>
2023-08-04 14:55:45 +0700

    starter

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline text-center">
+            Laravel 10 , Vue.js 3 , Tailwind CSS and Vite 2
+        </h1>
+    </div>
+</template>
+<script setup>
+import {ref} from 'vue'
+const name = ref();
+</script>
